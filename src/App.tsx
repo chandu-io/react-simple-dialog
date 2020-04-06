ba15051f5dddc0dc0aac7fa300e47fafb7e14a24
Chandrasekhar Thotakura <gitchandu@users.noreply.github.com>
2020-04-06 19:45:13 +0530

    Update btn txt

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
         className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
         onClick={() => setShow(false)}
       >
-        Close
+        Okay!
       </button>
     </div>
   );
